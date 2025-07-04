86d417ece59e0a8747436637b96b75841c3cbb7a
Mahlatse_Kganyago <kganyagoml@gmail.com>
2025-07-04 14:53:58 +0200

    Update install.R

--- install.R
+++ install.R
@@ -3,3 +3,11 @@ install.packages("rmarkdown")
 install.packages("httr")
 install.packages("shinydashboard")
 install.packages("leaflet")
+# -------------------Required libraries----------------------------
+# Install necessary packages if they are not already installed
+if (!requireNamespace("prospectr", quietly = TRUE)) install.packages("prospectr")
+if (!requireNamespace("ggplot2", quietly = TRUE)) install.packages("ggplot2")
+if (!requireNamespace("dplyr", quietly = TRUE)) install.packages("dplyr")
+if (!requireNamespace("tidyverse", quietly = TRUE)) install.packages("tidyverse")
+if (!requireNamespace("tidyr", quietly = TRUE)) install.packages("tidyr")
+if (!requireNamespace("skimr", quietly = TRUE)) install.packages("skimr")
